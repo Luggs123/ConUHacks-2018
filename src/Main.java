1dc58422b5c4de78c5a7b47a0c0d38a17dcc5660
Arthur <arthur.rakov@gmail.com>
2018-01-27 19:38:18 -0500

    preliminary javaFX UI done

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,85 @@
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneInput;
-import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
+
+	private static JTextArea inputField;
+	private static JTextField filePath;
+	private static void showWindow() {
+		JFrame frame = new JFrame("Mood Interpreter");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(900, 450);
+		frame.getContentPane().setLayout(null);
+		
+		inputField = new JTextArea();
+		inputField.setBounds(57, 56, 395, 286);
+		frame.getContentPane().add(inputField);
+		inputField.setColumns(10);
+		
+		JButton fileImport = new JButton("Import...");
+		fileImport.setBounds(57, 18, 97, 25);
+		frame.getContentPane().add(fileImport);
+		
+		filePath = new JTextField();
+		filePath.setBounds(182, 19, 270, 22);
+		frame.getContentPane().add(filePath);
+		filePath.setColumns(10);
+		
+		JButton sentenceSelect = new JButton("Sentence Mode");
+		sentenceSelect.setBounds(57, 355, 165, 25);
+		frame.getContentPane().add(sentenceSelect);
+		
+		JButton editMode = new JButton("Edit Mode");
+		editMode.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		editMode.setBounds(287, 355, 165, 25);
+		frame.getContentPane().add(editMode);
+		
+		JButton textAnalyze = new JButton("Analyze");
+		textAnalyze.setBounds(607, 18, 97, 25);
+		frame.getContentPane().add(textAnalyze);
+		
+		JPanel toneContainer = new JPanel();
+		toneContainer.setBounds(534, 56, 245, 286);
+		frame.getContentPane().add(toneContainer);
+		toneContainer.setLayout(null);
+		
+		JLabel[] tones = new JLabel[7];
+		
+		for (int i = 0; i < 7; i++) {
+			tones[i] = new JLabel("");
+			tones[i].setBounds(22, 57 + 19*i, 211, 16);
+			toneContainer.add(tones[i]);
+		}
+		
+		frame.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		JSONObject credentials = readJSONFile("res/credentials.json");
-
-		String username = credentials.getJSONObject("tone_analyzer").getJSONObject("credentials").getString("username");
-		String password = credentials.getJSONObject("tone_analyzer").getJSONObject("credentials").getString("password");
-
-		ToneAnalyzer service = new ToneAnalyzer("2017-09-21");
-		service.setUsernameAndPassword(username, password);
-
-		ToneInput toneInput = new ToneInput.Builder()
-				.text("meme").build();
-		ToneOptions options = new ToneOptions.Builder()
-				.toneInput(toneInput).build();
-
-		ToneAnalysis tone = (ToneAnalysis) service.tone(options).execute();
-
+		launch(args);
 	}
 
-	public static JSONObject readJSONFile(String filePath) {
-		try {
-			JSONObject obj = new JSONObject(new JSONTokener(new FileInputStream(filePath)));
-			return obj;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("View/mainView.fxml"));
+
+		primaryStage.setTitle("Mood Interpreter");
+		primaryStage.setScene(new Scene((Parent) loader.load()));
+
+
+		MainViewController controller = (MainViewController) loader.getController();
+		controller.setStage(primaryStage);
+
+		primaryStage.show();
+
 	}
 }
